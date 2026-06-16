3e12485f93bd5d3b41c2c5bcec7251c8c2d013b9
sifayet007 <sifayet2004@gmail.com>
2026-06-16 18:14:38 +0600

    Add Clarity tracking script to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,16 @@ export default function RootLayout({
       >
         {children}
       </body>
+
+      <Script id="clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "x7w5ocivro");
+        `}
+      </Script>
     </html>
   );
 }

4609e3c68200f8912f253b4811bc021d7022870e
sifayet007 <sifayet2004@gmail.com>
2026-06-16 18:49:28 +0600

    Improve formatting and organization of Google Analytics and Microsoft Clarity scripts in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,14 +28,31 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SBVCGGZ7LJ"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-SBVCGGZ7LJ');
+    `}
+        </Script>
+
+        {/* Microsoft Clarity */}
         <Script id="clarity" strategy="afterInteractive">
           {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "x7w5ocivro");
-  `}
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "x7w5ocivro");
+    `}
         </Script>
 
         {children}

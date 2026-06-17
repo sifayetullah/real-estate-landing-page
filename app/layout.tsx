a9829d5a0aeac2325e3dd430c0d34a92fdaf1c13
sifayet007 <sifayet2004@gmail.com>
2026-06-17 11:26:49 +0600

    update clarity id

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,14 +45,15 @@ export default function RootLayout({
         </Script>
 
         {/* Microsoft Clarity */}
-        <Script id="clarity" strategy="afterInteractive">
+        <Script id="ms-clarity-script" strategy="afterInteractive">
           {`
-      (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "x7w5ocivro");
-    `}
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x7w5ocivro");
+  `}
         </Script>
 
         {children}
